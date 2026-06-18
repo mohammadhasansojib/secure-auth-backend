9157da14c206feb0c276b8dc41b77b3261316b93
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-18 07:14:35 +0600

    feat: add global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import type { Request, Response } from "express"
+import type { NextFunction, Request, Response } from "express"
 
 
 const app = express();
@@ -12,6 +12,18 @@ app.use(express.text());
 // routes
 app.get('/', (req: Request, res: Response) => {
     res.send("Server running...")
+})
+
+
+// global error handler
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || 500;
+
+    res.status(status).json({
+        success: false,
+        message: err.message || "Something went wrong!",
+        code: err.code || "INTERNAL_SERVER_ERROR",
+    })
 })
 
 

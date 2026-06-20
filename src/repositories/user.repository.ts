62b1acdbe89468bb6925cba427a586c629d29ec3
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-21 00:05:06 +0600

    feat: add getUserByEmail & storeRefreshTokenIntoDB method in user repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -54,4 +54,48 @@ export class userRepository {
         }
     }
 
+    async getUserByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            }
+        });
+
+        if (!user) {
+            logger.info("user not found", {
+                email,
+            });
+            throw new NotFoundError("user not found");
+        }
+
+        return user;
+    }
+
+    async storeRefreshTokenIntoDB(payload: {
+        tokenHash: string,
+        sessionId: string,
+        userId: number,
+        expiresAt: Date,
+    }) {
+        // store refresh token into db
+        const refreshToken = await prisma.refreshToken.create({
+            data: {
+                tokenHash: payload.tokenHash,
+                sessionId: payload.sessionId,
+                userId: payload.userId,
+                expiresAt: payload.expiresAt,
+            }
+        });
+
+        if (!refreshToken) {
+            logger.error("something went wrong, can't store refresh token into db.");
+            throw new AppError("can't store refresh token into db");
+        }
+        logger.info("stored user session info into db", {
+            userId: payload.userId,
+        });
+
+        return refreshToken;
+    }
+
 }

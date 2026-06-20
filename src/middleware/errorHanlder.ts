36a0ee1d8caa6384fdddce1271007ce100d34a6f
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-20 22:27:27 +0600

    feat: update error handler methods
    add error object with error handler methods

--- src/middleware/errorHanlder.ts
+++ src/middleware/errorHanlder.ts
@@ -4,54 +4,61 @@ import { ErrorCode } from "../types/errorType.js";
 export class AppError extends Error {
     public status: number;
     public code: ErrorCode;
+    public error: any;
 
     constructor(
         message: string = "Something went wrong!",
+        error: any = {},
         status: number = 500,
         code: ErrorCode = "INTERNAL_SERVER_ERROR",
     ) {
         super(message);
         this.status = status;
         this.code = code;
+        this.error = error;
     }
 }
 
 export class NotFoundError extends AppError {
     constructor(
         message: string = "Not Found",
+        error: any = {},
         status: number = 404,
         code: ErrorCode = "NOT_FOUND",
     ) {
-        super(message, status, code);
+        super(message, error, status, code);
     }
 }
 
 export class ForbiddenError extends AppError {
     constructor(
         message: string = "Forbidden",
+        error: any = {},
         status: number = 403,
         code: ErrorCode = "FORBIDDEN",
     ) {
-        super(message, status, code);
+        super(message, error, status, code);
     }
 }
 
 export class AuthorizationError extends AppError {
     constructor(
         message: string = "Unauthorized Access",
+        error: any = {},
         status: number = 401,
         code: ErrorCode = "UNAUTHORIZED",
     ) {
-        super(message, status, code);
+        super(message, error, status, code);
     }
 }
 
 export class BadRequestError extends AppError {
     constructor(
         message: string = "Bad request",
+        error: any = {},
         status: number = 400,
         code: ErrorCode = "BAD_REQUEST",
     ) {
-        super(message, status, code);
+        super(message, error, status, code);
     }
 }

480bbe428427f4d34049eea6ba2c823ba178b3b6
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-06-20 23:52:29 +0600

    feat: add register controller function and complete user registration feature

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,12 +1,35 @@
 import type { Request, Response } from "express";
+import userService from "../services/user.service.js";
+import crypto from "crypto"
+import { cookieConfig } from "../utils/getCookieConfig.js";
+import { CookieConfig } from "../types/cookieConfigType.js";
+import { logger } from "../utils/logging.js";
 
 
 const register = async (req: Request, res: Response) => {
+    // collect data from req.body object
+    const {username, email, age, password, role} = req.body;
 
+    // create user
+    const user = await userService.createUser(
+        username,
+        email,
+        password,
+        age,
+        role,
+    );
+
+    // successfull response
+    res.status(201).json({
+        success: true,
+        message: "registration successfully completed",
+        data: {
+            user,
+        }
+    });
 }
 
 const login = async (req: Request, res: Response) => {
-
 }
 
 const refresh = async (req: Request, res: Response) => {
